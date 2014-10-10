d45d8879885a418108c6f27ebc77258be24c74ed
Alexey <alexey.lazarenko@jelastic.com>
2014-10-10 11:05:26 +0300

    poweradmin dir

---
+++ versions/3.6.1/poweradmin/inc/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_host		= 'localhost';
+$db_user		= 'root';
+$db_pass		= 'password';
+$db_name		= 'powerdns';
+$db_type		= 'mysql';
+$db_layer		= 'PDO';
+
+$session_key		= 'X5Jd+!QGDos!ZdDF0WDI@3ct$}RrfcL~Yp25WMi9HGij*n';
+
+$iface_lang		= 'en_EN';
+
+$dns_hostmaster		= 'powerdns';
+$dns_ns1		= '8.8.8.8';
+$dns_ns2		= '';
